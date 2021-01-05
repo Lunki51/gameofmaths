b0a56eb6b4641e8cecb4b8507538719db925417a
Chokearth <chokearth@gmail.com>
2021-01-05 17:32:46 +0100

    Teacher API: - Creation de l'API - Ajout de l'url pour changer son adresse mail

--- Application/gameofmath-webapp/routes/api.js
+++ Application/gameofmath-webapp/routes/api.js
@@ -3,7 +3,9 @@ const router = express.Router()
 
 const userRouter = require('./user')
 const quizRouter = require('./quiz')
+const teacherRouter = require('./teacher')
 router.use('/user/', userRouter)
 router.use('/quiz/', quizRouter)
+router.use('/teacher/', teacherRouter)
 
 module.exports = router;
